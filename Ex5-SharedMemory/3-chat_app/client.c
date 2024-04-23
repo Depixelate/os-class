fd364dc5c965277a1ca51cd1b3e88f1b57951d66
Sukessh Velusamy <sukesshvelusamy@gmail.com>
2024-04-23 06:59:07 +0530

    Finished Ex5.

--- Ex5-SharedMemory/3-chat_app/client.c
+++ Ex5-SharedMemory/3-chat_app/client.c
@@ -19,25 +19,25 @@ void perrorc(const char *msg) {
 
 bool waiting = false;
 int pos = 0;
-char *buf = NULL;
+int *buf = NULL;
 char *msgs = NULL;
 int shmid = 0;
 int server_pid = -1;
 
-void on_exit(int signal) {
-	signal(SIGINT, on_exit);
+void on_exit_func(int sig) {
+	signal(SIGINT, on_exit_func);
 	shmdt(buf);
 	shmctl(shmid, IPC_RMID, NULL);
 	exit(0);
 }
 
-void on_recieve_msg(int signal) {
-	signal(SIGINFO, on_recieve_msg);
+void on_recieve_msg(int sig) {
+	signal(SIGIO, on_recieve_msg);
 	char msg[BUF_SIZE];
 	for(int i = 0; true; i++) {
-		msg[i] = buf[pos];
+		msg[i] = msgs[pos];
 		int new_pos = (pos + 1) % BUF_SIZE;
-		if(buf[pos] == '\n') {
+		if(msgs[pos] == '\n') {
 			pos = new_pos;
 			msg[i+1] = '\0';
 			break;
@@ -51,31 +51,42 @@ void on_recieve_msg(int signal) {
 
 
 
-
-
 int main() {
-	signal(SIGINT, on_exit);
-	signal(SIGINFO, on_recieve_msg);
-	
-	
-	
+	signal(SIGINT, on_exit_func);
+	signal(SIGIO, on_recieve_msg);
+
+	key_t key = ftok("/tmp", 65);
+
+	shmid = shmget(key, BUF_SIZE, IPC_CREAT | 0777);
+	buf = (int *)shmat(shmid, NULL, 0);
+	buf[0] = getpid();
+	buf[1] = 0;
+	msgs = (char *)(&buf[2]);
+
+	while(buf[1] == 0)
+		;
+
+	server_pid = buf[1];
+
 	while(true) {
 		while(waiting)
 		;
 		printf("Do you want to quit(y/n): ");
 		char c = getchar();
-		if (c == 'y') {
+		getchar();
+		if (c == 'y')
+		{
 			kill(server_pid, SIGINT);
-			on_exit();
+			on_exit_func(0);
 		}
 		printf("Enter a message: ");
 		while(true) {
 			char c = getchar();
-			buf[pos] = c;
+			msgs[pos] = c;
 			pos = (pos + 1) % BUF_SIZE;
 			if(c == '\n') break;
 		}
-		kill(server_pid, SIGINFO);
+		kill(server_pid, SIGIO);
 		waiting = true;
 	}
 }
--- Ex5-SharedMemory/3-chat_app/server.c
+++ Ex5-SharedMemory/3-chat_app/server.c
@@ -3,51 +3,91 @@
 #include <sys/shm.h>
 #include <sys/ipc.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdlib.h>
 #include <stdbool.h>
-
-void perrorc(const char *msg) {
-    perror(msg);
-    exit(1);
-}
+#include <stdlib.h>
 
 #define BUF_SIZE 4096
 
-int main() {
-	key_t key = ftok("/tmp", 65);
-	int id = shmget(key, BUF_SIZE, 0);
-	if (id < 0) perrorc("An error occurred while getting the shared memory segment");
-	char *buf = (char *)shmat(id, NULL, 0);
-	if((int)buf == -1) perrorc("An error occurred while attaching to the shared memory segment");
-	
-	char *file_name = buf + 1;
-	char *content = file_name + strlen(file_name) + 1;
-	int *size_ptr = (int *)content;
-	content = content + 4;
-	int fd = open(file_name, O_RDONLY);
-	if(fd == -1) {
-		buf[0] = -1;
-		perrorc("An error occurred while reading the file");
-	}
-	
-	printf("File Content: \n");
-	
-	for(int i = 0; true; i++) {
-		int res = read(fd, content + i, 1);
-		if (res == 0) break;
-		if (res == -1) {
-			buf[0] = -1;
-			perrorc("An error occurred while reading the file");	
+void perrorc(const char *msg)
+{
+	perror(msg);
+	exit(1);
+}
+
+bool waiting = true;
+int pos = 0;
+int *buf = NULL;
+char *msgs = NULL;
+int shmid = 0;
+int client_pid = -1;
+
+void new_oe(int sig)
+{
+	signal(SIGINT, new_oe);
+	shmdt(buf);
+	//shmctl(shmid, IPC_RMID, NULL);
+	exit(0);
+}
+
+void on_recieve_msg(int sig)
+{
+	signal(SIGIO, on_recieve_msg);
+	char msg[BUF_SIZE];
+	for (int i = 0; true; i++)
+	{
+		msg[i] = msgs[pos];
+		int new_pos = (pos + 1) % BUF_SIZE;
+		if (msgs[pos] == '\n')
+		{
+			pos = new_pos;
+			msg[i + 1] = '\0';
+			break;
 		}
-		putchar(buf[i]);
-		*size_ptr += 1;
+		pos = new_pos;
 	}
-	
-	buf[0] = 1;
-	close(fd);
-	int result = shmdt(buf);
-	if (result < 0) perrorc("An error occurred while detaching from the shared memory segment");
+
+	printf("Message from server: %s", msg);
+	waiting = false;
+}
+
+int main()
+{
+	signal(SIGINT, new_oe);
+	signal(SIGIO, on_recieve_msg);
+
+	key_t key = ftok("/tmp", 65);
+
+	shmid = shmget(key, BUF_SIZE, 0);
+	buf = (int *)shmat(shmid, NULL, 0);
+	client_pid = buf[0];
+	buf[1] = getpid();
+	msgs = (char *)(&buf[2]);
+	while (true)
+	{
+		while (waiting)
+			;
+		printf("Do you want to quit(y/n): ");
+		char c = getchar();
+		getchar();
+		if (c == 'y')
+		{
+			kill(client_pid, SIGINT);
+			new_oe(0);
+		}
+		printf("Enter a message: ");
+		while (true)
+		{
+			char c = getchar();
+			msgs[pos] = c;
+			pos = (pos + 1) % BUF_SIZE;
+			if (c == '\n')
+				break;
+		}
+		kill(client_pid, SIGIO);
+		waiting = true;
+	}
 }
